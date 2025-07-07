0fcea8432027c7639d137c2f31212d1e61cbaa09
SzaszAndrasGabor <162968444+SzaszAndrasGabor@users.noreply.github.com>
2025-07-07 16:32:32 +0200

    Create Category.php

---
+++ app/Models/Category.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Models;
+global $pdo;
+
+class Category {
+    public static function getAll() {
+        global $pdo;
+        $stmt = $pdo->query("SELECT * FROM categories");
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
+?>
